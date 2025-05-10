e59e024e58cf1285f64558660aac1850820549e8
Sagar Sengupta <sagarsengupta2009@gmail.com>
2025-05-10 19:57:24 +0530

    first commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,23 @@
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+function Modal({ open, children }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, []);
+
+  return createPortal(
+    <dialog className="modal" ref={dialog} open={open}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
+
+export default Modal;
